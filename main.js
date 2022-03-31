17a3e68435c6a553638f8f0d300a7b15c37592b3
deenamohdous <dina.mohdous@gmail.com>
2022-03-31 22:39:12 +0300

    still

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-console.log(arrowTitle)
+
 /* START CODE UNDER THIS LINE */
+//from  container 1  press start to begin the game 
+// it will take you to container 2 (link start  button with con 2 )
+//  make an array with movies ele 
+// array wieh letter 
+//  loop ---> creat button <ele>(id)
+// 
+//
+const array=[ "A", "B" ," C", " D","E" ,"F" , "G","H" , "I", "J","K" ,"L" , "M", "N", "O", "P", "Q","R","S" ,"T" , "U", "V","W" , "X", "Y", "Z"];
+// [''vikings'', '“'friends”, “New Amsterdam”, “the good doctor” , “dark” , “Chernobyl”, “breaking bad” , “when they see us “ , “banshee”, “Unbelievable”, “lupin”
+const series=["vikings"]
+
+array.forEach(element => {
+     const button = document.createElement('button')
+    button.innerText=element
+//    console.log(button);
+
+   button.append()
+}); 
+   
