ec86fd9f99f98c6361a921e738d05309928888cc
deenamohdous <dina.mohdous@gmail.com>
2022-03-31 22:50:48 +0300

    still

--- main.js
+++ main.js
@@ -5,8 +5,8 @@
 //  make an array with movies ele 
 // array wieh letter 
 //  loop ---> creat button <ele>(id)
-// 
-//
+// append button in body 
+
 const array=[ "A", "B" ," C", " D","E" ,"F" , "G","H" , "I", "J","K" ,"L" , "M", "N", "O", "P", "Q","R","S" ,"T" , "U", "V","W" , "X", "Y", "Z"];
 // [''vikings'', '“'friends”, “New Amsterdam”, “the good doctor” , “dark” , “Chernobyl”, “breaking bad” , “when they see us “ , “banshee”, “Unbelievable”, “lupin”
 const series=["vikings"]
@@ -15,7 +15,9 @@ array.forEach(element => {
      const button = document.createElement('button')
     button.innerText=element
 //    console.log(button);
-
-   button.append()
+button.id="third"
+const body = document.querySelector("body");
+   body.append(button)
 }); 
    
+//make 
