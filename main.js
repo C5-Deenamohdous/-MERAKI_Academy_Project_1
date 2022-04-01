1b6b4ea926561b55c3f35b6166ffd4cfcfcdeb17
deenamohdous <dina.mohdous@gmail.com>
2022-04-01 21:37:14 +0300

    working

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /* START CODE UNDER THIS LINE */
-const gamecontainer = document.querySelector(".container");
-console.log(gamecontainer);
+// const gamecontainer = document.querySelector(".container");
+// console.log(gamecontainer);
 //from  container 1  press start to begin the game
 // it will take you to container 2 (link start  button with con 2 )
 
@@ -39,61 +39,76 @@ const array = [
   "Z",
 ];
 // [''vikings'', '“'friends”, “New Amsterdam”, “the good doctor” , “dark” , “Chernobyl”, “breaking bad” , “when they see us “ , “banshee”, “Unbelievable”, “lupin”
+//!------- make start button 
+// const cont= document.querySelector("#container");
+// const fpage= document.querySelector("#p1");
+// const SecondPage= document.querySelector("#p2");
+// const start=document.querySelector("#p3");
+
+// const gameFunc=()=>{
+//   firstpage.style.disply="none";
+//   secpage.style.display="block";
+ 
+// }
+// start.addEventListener("click",gameFunc );
 
 //! arrayfor each button
 
 array.forEach((element , index) => {
-  const container=document.createElement("div")
+
   const button = document.createElement("button");
   button.innerText = element;
-    button.class = "button";
-  container.append(button);
-  gamecontainer.append(container)
-
+  const body = document.querySelector("body");
+  body.append(button);
   button.style.color = "red";
   button.style.width = "30px";
-
-});
-//}
-
-//! array for series to become randomly
-const series = ["vikings", "banshee"];
-//make new array called series
-// every element in array is come randomly
-
-const random = Math.floor(Math.random() * series.length);
-console.log(random);
-
-//loop ------length of ele = numbers of bottom borders
-// convert ele to split array
-
-series.forEach((element) => {
-  element.split("");
-  console.log( element.split(""));
+  //    button.style.alignItems
   
-  // create a p tag to put element
-  const p = document.createElement("p");
-  const body = document.querySelector("body");
-  p.innerText = element;
-  p.class = "ele";
-  body.append(p);
-//  p.style.display = "none";
+});
+// const letterButton=document.querySelector("button")
+// letterButton.addEventListener("click", buttonFunction)
+// //}
+// const buttonFunction=()=>{
 
-//   console.log(p);
+// }
+// //! array for series to become randomly
+// const series = ["vikings", "banshee"];
+// //make new array called series
+// // every element in array is come randomly
 
-  // //! create border tag to contain each letter ??????
-   // element.length=Borders
-//   console.log(Borders);  
+// const random = Math.floor(Math.random() * series.length);
+// console.log(random);
+
+// //loop ------length of ele = numbers of bottom borders
+// // convert ele to split array
+
+// series.forEach((element) => {
+//   element.split("");
+//   console.log( element.split(""));
+  
+//   // create a p tag to put element
+//   const p = document.createElement("p");
+//   const body = document.querySelector("body");
+//   p.innerText = element;
+//   p.class = "ele";
+//   body.append(p);
+// //  p.style.display = "none";
+
+// //   console.log(p);
+
+//   // //! create border tag to contain each letter ??????
+//    // element.length=Borders
+// //   console.log(Borders);  
    
-for (let i = 0; i < element.length; i++) { 
+// for (let i = 0; i < element.length; i++) { 
  
- const  Borders = document.createElement('border')
- const body = document.querySelector("body");
- Borders.class="lines"
-  Borders.innerText="_____ ";
-  body.append(Borders)
-}
- });
+//  const  Borders = document.createElement('border')
+//  const body = document.querySelector("body");
+//  Borders.class="lines"
+//   Borders.innerText="_____ ";
+//   body.append(Borders)
+// }
+//  });
 
  //!-------- how to defined button inside first loop  ????? 
  //!--------- why the image is not appear in body ?????
